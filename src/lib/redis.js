41bad23633b9b95a3d55a3d67e6f02a7fbd25848
Ratama Adhi Nugraha <ratama.adhi@gmail.com>
2025-04-23 01:59:47 +0700

    fix: redis 2

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -7,7 +7,14 @@ if (process.env.NODE_ENV === 'production') {
   redis = new Redis(process.env.REDIS_URL, {
     // optional: log friendly name
     name: 'vercel-prod-redis',
-    tls: process.env.REDIS_URL?.startsWith('rediss://') ? {} : undefined,
+    // tls: process.env.REDIS_URL?.startsWith('rediss://') ? {} : undefined,
+    reconnectOnError(err) {
+      const targetError = "READONLY";
+      if (err.message.includes(targetError)) {
+        // Only reconnect when the error contains "READONLY"
+        return true; // or `return 1;`
+      }
+    },
   })
 } else {
   // ✅ Local development
