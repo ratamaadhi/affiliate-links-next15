7de0ac01c2178c30558cc29fb0f7105def15fb8d
Ratama Adhi <ratama.adhi@gmail.com>
2025-08-25 22:54:05 +0700

    feat: add create and list pages functionality with dialog for page creation; implement slug generation and loading states

--- src/server/pages.ts
+++ src/server/pages.ts
@@ -4,11 +4,33 @@ import { auth } from '@/lib/auth';
 import db from '@/lib/db';
 import { PageInsert, page } from '@/lib/db/schema';
 import { eq } from 'drizzle-orm';
+import { customAlphabet } from 'nanoid';
 import { headers } from 'next/headers';
+import slugify from 'slug';
 
-export const createPage = async (values: PageInsert) => {
+const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 5);
+
+export const createPage = async (values: Omit<PageInsert, 'slug'>) => {
+  let slug = slugify(values.title);
+  let existingSlug = !!(await db.query.page.findFirst({
+    where: eq(page.slug, slug),
+  }));
+
+  while (existingSlug) {
+    const ids = nanoid();
+    const idSlug = `${slug}-${ids}`;
+    existingSlug = !!(await db.query.page.findFirst({
+      where: eq(page.slug, idSlug),
+    }));
+
+    if (!existingSlug) {
+      slug = idSlug;
+      break;
+    }
+  }
+
   try {
-    await db.insert(page).values(values);
+    await db.insert(page).values({ ...values, slug });
     return { success: true, message: 'Page created successfully' };
   } catch {
     return { success: false, message: 'Failed to create page' };
@@ -55,9 +77,13 @@ export const getPageById = async (id: number) => {
   }
 };
 
-export const updatePage = async (id: number, values: PageInsert) => {
+export const updatePage = async (id: number, values: Partial<PageInsert>) => {
   try {
-    await db.update(page).set(values).where(eq(page.id, id));
+    const newValues: Partial<PageInsert> = { ...values };
+    if (values.title) {
+      newValues.slug = values.title.toLowerCase().replace(/\s+/g, '-');
+    }
+    await db.update(page).set(newValues).where(eq(page.id, id));
     return { success: true, message: 'Page updated successfully' };
   } catch {
     return { success: false, message: 'Failed to update page' };
